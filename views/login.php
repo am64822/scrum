b6fdd224f7e0278edc8af65249872f85dd8b2d71
AM <68774440+am64822@users.noreply.github.com>
2022-03-04 22:34:02 +0300

    Delete views directory

--- views/login.php
+++
@@ -1,7 +0,0 @@
-<div id='loginFrame' class='loginFrame'>
-    <div>Пользователь</div><input type='text' id='user'>
-    <br>
-    <div>Пароль</div><input type='password' id='pwd'>
-    <br>
-    <div>&nbsp;</div><button type='button' id='submit'>OK</button>
-</div>
