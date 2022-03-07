d751ea5af6d1ba17210788d877d238a16c61328a
SyominAlex <60996055+SyominAlex@users.noreply.github.com>
2022-03-08 01:01:18 +0300

    on dev changed color to blue
    extended description: on dev changed color to blue

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function displayError(comment, element, result) { // отмена изменения значения. Очистка поля результата. Цветовая индикация
-    comment.style.color = 'crimson';
-    element.style.color = 'crimson';
+    comment.style.color = 'blue';
+    element.style.color = 'blue';
 
     result.innerText = '';
     
